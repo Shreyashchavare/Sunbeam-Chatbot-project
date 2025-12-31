8ab583b390420eb2a770251eefdcdcd059e1e835
Shreyash Chavare <shreyashchavare3110@gmail.com>
2025-12-31 17:50:45 +0530

    feat: add requirement.txt and refactor: knowledge

--- knowledge/vectorstore.py
+++ knowledge/vectorstore.py
@@ -1,5 +1,6 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+
 def get_vectorstore(persist_dir="chroma_db"):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
